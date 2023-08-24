95758830aca15921a95cfbb24e27ec34bea4a780
MatiasBSC <mati.sandoval@duocuc.cl>
2023-08-23 21:41:43 -0400

    notificaciones

--- Panda-2/src/app/tab5/tab5.page.ts
+++ Panda-2/src/app/tab5/tab5.page.ts
@@ -6,10 +6,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tab5.page.scss'],
 })
 export class Tab5Page implements OnInit {
+  notifications: any[] = [
+    {
+      userAvatarUrl: 'https://i.pinimg.com/564x/a9/a5/b7/a9a5b7f1fc099ab24cf89f9fbd62b1e4.jpg',
+      title: 'Rukia',
+      message: 'Te ha comenzado a seguir',
+      fecha: '20:03 29/08/2023',
+    },
+    {
+      userAvatarUrl: 'https://i.pinimg.com/564x/ce/31/08/ce31080c26caea0bc51f391bc520db2d.jpg',
+      title: 'Mirai',
+      message: 'Le dio me gusta a la imagen que compartiste',
+      fecha: '21:33 23/08/2023',
+    },
+    {
+      userAvatarUrl: 'https://i.pinimg.com/236x/5f/db/01/5fdb0188dd857129a432b6a0bc875cc7.jpg',
+      title: 'Kallen',
+      message: 'Le dio me gusta a la imagen que compartiste',
+      fecha: '21:33 23/08/2023',
+    },
+    {
+      userAvatarUrl: 'https://i.pinimg.com/236x/1a/8e/aa/1a8eaa790174f889b856a40c17271b3a.jpg',
+      title: 'Hinata',
+      message: 'Le dio me gusta a la imagen que compartiste',
+      fecha: '21:33 23/08/2023',
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
